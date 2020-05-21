0cbad6e7d4874eed2a3b734ab89cb8f7b7844407
KerronKing <king_of_palmiste@hotmail.com>
2020-05-21 19:54:41 -0400

    chore: set up page router, template emgine and home view

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const wikiPageRouter = require('./routes/wikiPages');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/wikiPage', wikiPageRouter);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(3000);
