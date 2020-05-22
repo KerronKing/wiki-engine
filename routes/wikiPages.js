ce6dd30d7e2d0155dc8cb1269820f37a8321d2e9
KerronKing <king_of_palmiste@hotmail.com>
2020-05-22 12:35:27 -0400

    chore: Add page model and mongoose logic

--- routes/wikiPages.js
+++ routes/wikiPages.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const WikiPage = require('../models/wikiPage');
 
 const router = express.Router();
 
@@ -7,15 +8,25 @@ router.get('/index', (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  res.render('wikiPages/new');
+  res.render('wikiPages/new', { wikiPage: new WikiPage() });
 });
 
 router.get('/edit', (req, res) => {
   res.render('wikiPages/edit');
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const wikiPage = new WikiPage({
+    title: req.body.title,
+    content: req.body.content,
+  });
 
+  try {
+    await wikiPage.save();
+    res.redirect(`/wikiPages/${wikiPage.id}`);
+  } catch (err) {
+    res.render('wikiPages/new', { wikiPage });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const wikiPagesRouter = require('./routes/wikiPages');
 
 const app = express();
+mongoose.connect('mongodb://localhost/wikiengine',
+  { useNewUrlParser: true, useUnifiedTopology: true });
 app.set('view engine', 'ejs');
 
-app.use('/wikiPages', wikiPagesRouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.use('/wikiPages', wikiPagesRouter);
 app.listen(3000);
