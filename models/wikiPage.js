f1242d656436349a6eb1562f9b9d6c1a1b5ee2e4
KerronKing <king_of_palmiste@hotmail.com>
2020-05-23 07:06:36 -0400

    chore: add markdown conversion logic

--- models/wikiPage.js
+++ models/wikiPage.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const marked = require('marked');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const dompurify = createDomPurify(new JSDOM().window);
 
 const wikiPageSchema = new mongoose.Schema({
   title: {
@@ -19,15 +24,24 @@ const wikiPageSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
 });
 
-wikiPageSchema.pre('validate', next => {
+wikiPageSchema.pre('validate', function (next) {
   if (this.title) {
     this.slug = slugify(this.title, {
       lower: true,
       strict: true,
     });
   }
+
+  if (this.content) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.content));
+  }
+
   next();
 });
 
